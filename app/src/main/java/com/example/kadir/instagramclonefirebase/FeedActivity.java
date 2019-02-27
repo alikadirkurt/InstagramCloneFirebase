bb5ae3f9468a9feb3659bb8c1cbd94778ac6e8a7
murakadir12 <34250985+murakadir12@users.noreply.github.com>
2019-02-27 19:52:29 +0300

    upload

---
+++ app/src/main/java/com/example/kadir/instagramclonefirebase/FeedActivity.java
@@ -0,0 +1,33 @@
+package com.example.kadir.instagramclonefirebase;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class FeedActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_post,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add_post){
+            Intent intent = new Intent(getApplicationContext(),uploadActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
--- app/src/main/java/com/example/kadir/instagramclonefirebase/MainActivity.java
+++ app/src/main/java/com/example/kadir/instagramclonefirebase/MainActivity.java
@@ -1,13 +1,81 @@
 package com.example.kadir.instagramclonefirebase;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    EditText emailText;
+    EditText paswordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailText = findViewById(R.id.emailText);
+        paswordText = findViewById(R.id.paswordText);
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+            Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+            startActivity(intent);
+        }
+
+    }
+
+    public void signin(View view){
+        mAuth.signInWithEmailAndPassword(emailText.getText().toString(),paswordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+    public void signup(View view){
+        mAuth.createUserWithEmailAndPassword(emailText.getText().toString(), paswordText.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                            //   FirebaseUser user = mAuth.getCurrentUser();
+                        }
+
+
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
